5cf61208bf580cc1567cb203258e2641b8849965
Landry King <lking@diversitech.com>
2024-10-31 15:56:37 -0400

    Added code for google maps API

---
+++ src/maps_testing/maps.py
@@ -0,0 +1,31 @@
+import urllib.request
+import webbrowser
+import urllib
+import json
+
+api_key = "AIzaSyBQi69vJT0EvL2A2mX8aiKPCoHhELcQQvc"
+type Coordinate = tuple[float, float]
+type Sizing = tuple[float, float]
+def construct_request(location: Coordinate, size: Sizing, fov: int, key: str = api_key):
+    template = "https://maps.googleapis.com/maps/api/streetview?location={},{}&size={}x{}&fov={}&key={}"
+    return template.format(location[0], location[1], size[0], size[1], fov, key)
+
+def geolocate(address: str, key: str = api_key):
+    template = "https://maps.googleapis.com/maps/api/geocode/json?address={}&key={}"
+    formatted_address = address.replace(" ", "+")
+    final = template.format(formatted_address, key)
+
+    request = urllib.request.urlopen(final)
+    json_data = json.loads(request.read())
+
+    location = (json_data["results"][0]["geometry"]["location"]["lat"], json_data["results"][0]["geometry"]["location"]["lng"])
+    return location
+
+def main():
+    location = geolocate("9538 Kingston Crossing Cir., Johns Creek, GA")
+    print(location)
+    request = construct_request(location = location, size = (600, 480), fov = 120)
+    webbrowser.open(request)
+
+if __name__ == "__main__":
+    main()
